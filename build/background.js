2850064ded99d401bb85208f694216dfaef0ebdb
GovernorFancyPants <tobiasjohansson123@gmail.com>
2014-10-13 00:47:22 +0200

    Removed build folder to honor gitignore

--- build/background.js
+++
@@ -1,7 +0,0 @@
-// chrome.extension.onConnect.addListener(function(port) {
-//     console.log("Connected .....");
-//     port.onDisconnect.addListener(function(msg) {
-//         console.log("message recieved" + msg);
-//         port.postMessage("balls");
-//     });
-// });
